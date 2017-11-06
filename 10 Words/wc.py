584389f9f3b26eb703b655a3cbb605093c6ed9cd
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2017-11-06 12:20:43 -0500

    Add docstrings and fix case bug

--- 10 Words/wc.py
+++ 10 Words/wc.py
@@ -2,6 +2,7 @@
 
 import collections
 import os
+import pprint
 
 
 def remove_nonalpha(w):
@@ -40,23 +41,31 @@ def count_words_in_file(name):
 
 
 def create_markov_chain(list_of_words):
-    dict = collections.defaultdict(lambda: [])
+    '''
+    input: list_of_words - list-like of string-like representing a list of words in order used
+    output: dictionary with the word as key and list of possible next words as value
+    '''
+    d = collections.defaultdict(lambda: [])
 
     for i in range(0, len(list_of_words) - 1):
-        dict[list_of_words[i]].append(list_of_words[i + 1])
+        d[list_of_words[i]].append(list_of_words[i + 1])
 
-    return dict
+    return dict(d)
 
 
 def create_markov_chain_from_file(name):
+    '''
+    input: name - filename to create markov chain from
+    output: dictionary with the word as key and list of possible next words as value
+    '''
     # Get filename relative to module location
     filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), name)
 
     with open(filename) as file:
-        text = file.read()
+        text = file.read().lower()
         return create_markov_chain([remove_nonalpha(w) for w in text.split()])
 
 
 if __name__ == "__main__":
-    d = create_markov_chain_from_file("hamlet.txt")
-    print(d)
+    d = create_markov_chain_from_file("psalms.txt")
+    pprint.pprint(d)
