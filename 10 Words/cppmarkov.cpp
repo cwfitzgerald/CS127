ed293ae3aa8142c7f426ce73b1428ea6e6945c6f
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2017-11-08 19:24:41 -0500

    Add cpp implimentation of markov chain

---
+++ 10 Words/cppmarkov.cpp
@@ -0,0 +1,60 @@
+#include <cctype>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+using string_list = std::vector<std::string>;
+
+string_list split(const std::string string) {
+	std::vector<std::string> output;
+	std::string cache;
+	for (auto c : string) {
+		char lower = static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
+		if ('a' <= lower && lower <= 'z') {
+			cache.push_back(lower);
+		}
+		else if (cache.size() != 0) {
+			output.emplace_back(std::move(cache));
+			cache = std::string();
+		}
+	}
+
+	return output;
+}
+
+using markov = std::unordered_map<std::string, std::vector<std::string>>;
+
+__attribute__((noinline)) markov build_chain(const string_list& list) {
+	markov m;
+	for (std::size_t i = 0; i < list.size() - 1; ++i) {
+		m[list[i]].emplace_back(list[i + 1]);
+	}
+	return m;
+}
+
+int main() {
+	std::ifstream f("psalms.txt");
+	std::ostringstream ss;
+	ss << f.rdbuf();
+
+	auto string = ss.str();
+	auto split = ::split(string);
+
+	auto before = std::chrono::high_resolution_clock::now();
+	auto mark = build_chain(split);
+	auto after = std::chrono::high_resolution_clock::now();
+
+	for (auto const & [ key, list ] : mark) {
+		std::cout << key << ": ";
+		for (auto& word : list) {
+			std::cout << " " << word;
+		}
+		std::cout << '\n';
+	}
+
+	std::cout << (after - before).count() / 1000000.0 << "ms\n";
+}
