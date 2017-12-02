a11ae4471616eab854e1f00f7236519ba29259f8
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2017-12-02 17:38:02 -0500

    Change linting framework

--- lint.sh
+++ lint.sh
@@ -1,1 +1,9 @@
-@RUN_CLANG_TIDY_EXE@ '-header-filter=^@CMAKE_SOURCE_DIR@/@DIR_TO_LINT_STR@.*' -checks=*,-clang-analyzer-alpha.*,-clang-diagnostic-unknown-warning-option*,-clang-diagnostic-unknown-warning-option*,-clang-diagnostic-unused-command-line-argument,-llvm-header-guard -quiet @FILES_TO_LINT_STR@
+if [ $# -ge 1 ]; then
+	files="$@"
+else
+	files='@FILES_TO_LINT_STR@'
+fi
+
+echo $files
+
+@RUN_CLANG_TIDY_EXE@ '-header-filter=^@CMAKE_SOURCE_DIR@/@DIR_TO_LINT_STR@.*' -checks=*,-clang-analyzer-alpha.*,-clang-diagnostic-unknown-warning-option*,-clang-diagnostic-unknown-warning-option*,-clang-diagnostic-unused-command-line-argument,-llvm-header-guard -quiet $files
